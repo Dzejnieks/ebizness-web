419e5802d54668a829fdf022671be38468ee9df6
Dzejnieks <aksels.skvorcovs@gmail.com>
2020-01-10 07:59:53 +0200

    Teams changes

--- index.php
+++ index.php
@@ -1,143 +1,138 @@
-<?php 
+<?php
 session_start();
 //$connect = mysqli_connect("localhost", "root", "", "ebiz");
 $apiFile = file_get_contents("https://ebiznessvia.000webhostapp.com/api/shop/read.php");
-$json = json_decode($apiFile,true);
+$json = json_decode($apiFile, true);
 
-if(isset($_POST["add_to_cart"]))
-{
-	if(isset($_SESSION["shopping_cart"]))
-	{
-		$item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
-		if(!in_array($_GET["id"], $item_array_id))
-		{
-			$count = count($_SESSION["shopping_cart"]);
-			$item_array = array(
-				'item_id'			=>	$_GET["id"],
-				'item_name'			=>	$_POST["hidden_name"],
-				'item_price'		=>	$_POST["hidden_price"],
-				'item_quantity'		=>	$_POST["quantity"]
-			);
-			$_SESSION["shopping_cart"][$count] = $item_array;
-		}
-		else
-		{
-			echo '<script>alert("Item Already Added")</script>';
-		}
-	}
-	else
-	{
-		$item_array = array(
-			'item_id'			=>	$_GET["id"],
-			'item_name'			=>	$_POST["hidden_name"],
-			'item_price'		=>	$_POST["hidden_price"],
-			'item_quantity'		=>	$_POST["quantity"]
-		);
-		$_SESSION["shopping_cart"][0] = $item_array;
-	}
+if (isset($_POST["add_to_cart"])) {
+    if (isset($_SESSION["shopping_cart"])) {
+        $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+        if (!in_array($_GET["id"], $item_array_id)) {
+            $count = count($_SESSION["shopping_cart"]);
+            $item_array = array(
+                'item_id' => $_GET["id"],
+                'item_name' => $_POST["hidden_name"],
+                'item_price' => $_POST["hidden_price"],
+                'item_quantity' => $_POST["quantity"]
+            );
+            $_SESSION["shopping_cart"][$count] = $item_array;
+        } else {
+            echo '<script>alert("Item Already Added")</script>';
+        }
+    } else {
+        $item_array = array(
+            'item_id' => $_GET["id"],
+            'item_name' => $_POST["hidden_name"],
+            'item_price' => $_POST["hidden_price"],
+            'item_quantity' => $_POST["quantity"]
+        );
+        $_SESSION["shopping_cart"][0] = $item_array;
+    }
 }
 
-if(isset($_GET["action"]))
-{
-	if($_GET["action"] == "delete")
-	{
-		foreach($_SESSION["shopping_cart"] as $keys => $values)
-		{
-			if($values["item_id"] == $_GET["id"])
-			{
-				unset($_SESSION["shopping_cart"][$keys]);
-				echo '<script>window.location="index.php"</script>';
-			}
-		}
-	}
+if (isset($_GET["action"])) {
+    if ($_GET["action"] == "delete") {
+        foreach ($_SESSION["shopping_cart"] as $keys => $values) {
+            if ($values["item_id"] == $_GET["id"]) {
+                unset($_SESSION["shopping_cart"][$keys]);
+                echo '<script>window.location="index.php"</script>';
+            }
+        }
+    }
 }
 
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<script src="js.js" async></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	</head>
-	<body>
-		<br />
-		<div class="container">
-			<?php
-				for($x=0; $x<count($json["records"]);$x++){
-					$singleEntry = $json["records"][$x];
-				?>
-			<div class="col-md-4">
-				<form method="post" action="index.php?action=add&id=<?php echo $singleEntry["id"]; ?>">
-					<div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
-						<img src="<?php echo $singleEntry["image"]; ?>" class="img-responsive" /><br />
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <script src="js.js" async></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <br/>
+    <div class="container">
+        <?php
+        for ($x = 0; $x < count($json["records"]); $x++) {
+            $singleEntry = $json["records"][$x];
+            ?>
+            <div class="col-md-4">
+                <form method="post" action="index.php?action=add&id=<?php echo $singleEntry["id"]; ?>">
+                    <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;"
+                         align="center">
+                        <img src="<?php echo $singleEntry["image"]; ?>" class="img-responsive"/><br/>
 
-						<h4 class="text-info"><?php echo $singleEntry["name"]; ?></h4>
+                        <h4 class="text-info"><?php echo $singleEntry["name"]; ?></h4>
 
-						<h4 class="text-danger">$ <?php echo $singleEntry["price"]; ?></h4>
+                        <h4 class="text-danger">$ <?php echo $singleEntry["price"]; ?></h4>
 
-						<input type="text" name="quantity" value="1" class="form-control" />
+                        <input type="text" name="quantity" value="1" class="form-control"/>
 
-						<input type="hidden" name="hidden_name" value="<?php echo $singleEntry["name"]; ?>" />
+                        <input type="hidden" name="hidden_name" value="<?php echo $singleEntry["name"]; ?>"/>
 
-						<input type="hidden" name="hidden_price" value="<?php echo $singleEntry["price"]; ?>" />
+                        <input type="hidden" name="hidden_price" value="<?php echo $singleEntry["price"]; ?>"/>
 
-						<input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
+                        <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success"
+                               value="Add to Cart"/>
 
-					</div>
-				</form>
-			</div>
-			<?php
-				}
-			?>
-			<div style="clear:both"></div>
-			<br />
-			<h3>Order Details</h3>
-			<div class="table-responsive">
-				<table class="table table-bordered">
-					<tr>
-						<th width="40%">Item Name</th>
-						<th width="10%">Quantity</th>
-						<th width="20%">Price</th>
-						<th width="15%">Total</th>
-						<th width="5%">Action</th>
-					</tr>
-					<?php
-					if(!empty($_SESSION["shopping_cart"]))
-					{
-						$total = 0;
-						foreach($_SESSION["shopping_cart"] as $keys => $values)
-						{
-					?>
-					<tr>
-						<td><?php echo $values["item_name"]; ?></td>
-						<td><?php echo $values["item_quantity"]; ?></td>
-						<td>$ <?php echo $values["item_price"]; ?></td>
-						<td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
-						<td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
-					</tr>
-					<?php
-							$total = $total + ($values["item_quantity"] * $values["item_price"]);
-						}
-					?>
-					<tr>
-						<td colspan="3" align="right">Total</td>
-						<td align="right">$ <?php echo number_format($total, 2); ?></td>
-						<td><button id="btn-pay" class="btn btn-success" type="button">PURCHASE</button></td>
-					</tr>
-					<?php
-					}
-					?>
-						
-				</table>
-			</div>
-		</div>
-	</div>
-	<br />
-	</body>
-	<script src="store.js" async></script>
-</html>
+                    </div>
+                </form>
+            </div>
+            <?php
+        }
+        ?>
+        <div style="clear:both"></div>
+        <br/>
+        <h3>Order Details</h3>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <tr>
+                    <th width="40%">Item Name</th>
+                    <th width="10%">Quantity</th>
+                    <th width="20%">Price</th>
+                    <th width="15%">Total</th>
+                    <th width="5%">Action</th>
+                </tr>
+                <?php
+                if (!empty($_SESSION["shopping_cart"])) {
+                    $total = 0;
+                    foreach ($_SESSION["shopping_cart"] as $keys => $values) {
+                        ?>
+                        <tr>
+                            <td><?php echo $values["item_name"]; ?></td>
+                            <td><?php echo $values["item_quantity"]; ?></td>
+                            <td>$ <?php echo $values["item_price"]; ?></td>
+                            <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>
+                            <td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span
+                                            class="text-danger">Remove</span></a></td>
+                        </tr>
+                        <?php
+                        $total = $total + ($values["item_quantity"] * $values["item_price"]);
+                        $_SESSION['total'] = $total;
+                    }
+                    ?>
+                    <tr>
+                        <td colspan="3" align="right">Total</td>
+                        <td align="right">$ <?php echo number_format($total, 2); ?></td>
+                        <td>
+                            <button id="btn-pay" class="btn btn-success" type="button"
+                                    onclick="document.location.href='pay.php'">PURCHASE
+                            </button>
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ?>
+
+            </table>
+        </div>
+    </div>
+    </div>
+    <br/>
+    </body>
+    <script src="store.js" async></script>
+    </html>
 
 <?php
 //If you have use Older PHP Version, Please Uncomment this function for removing error 
